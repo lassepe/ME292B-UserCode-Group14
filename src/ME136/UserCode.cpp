62aa9f99da340af9da76e9ff7b86b02679d8990d
Jackie First <jacquelynfirst@gmail.com>
2018-11-06 07:28:51 -0800

    Added Optical Flow and Ranging board Printstatus info for Preparations

--- src/ME136/UserCode.cpp
+++ src/ME136/UserCode.cpp
@@ -121,6 +121,12 @@ void PrintStatus() {
     printf("\n");
     printf("================================================");
     printf("\n");
+    printf("Last_range_=_%6.3fm,_", \
+           double(lastMainLoopInputs.heightSensor.value));
+    printf("Last_flow:_x=%6.3,_y=%6.3f\n", \
+           double(lastMainLoopInputs.opticalFlowSensor.value_x), \
+           double(lastMainLoopInputs.opticalFlowSensor.value_y));
+    printf("\n");
     printf("Telemetry Channel Info");
     printf("\n");
     int i = 0;
